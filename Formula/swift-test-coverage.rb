a10c5bedf03373149ade2e55b2443c631eef957d
Steffen Kötte <steffen.koette@gmail.com>
2021-10-02 00:43:07 -0700

    Update swift-test-coverage.rb

--- Formula/swift-test-coverage.rb
+++ Formula/swift-test-coverage.rb
@@ -2,7 +2,7 @@ class SwiftTestCoverage < Formula
   desc "Command-line Tool to display the test coverage of a swift package"
   homepage "https://github.com/Nef10/swift-test-coverage"
   url "https://github.com/Nef10/swift-test-coverage/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "29567d0120c9d1800c22737b1c327c3a1ae30910f219210e6d531bdad0b2af86"
+  sha256 "ac0c58289cde648564373a743250752ebc71a7971bec710772e3e5fa9b9f6664"
   license "MIT"
 
   def install
