4eec70d87533da0b464118cfaf3fcb0b997df735
Steffen Kötte <steffen.koette@gmail.com>
2021-10-30 18:58:38 -0700

    Update domination.rb (#11)

--- Casks/domination.rb
+++ Casks/domination.rb
@@ -1,6 +1,6 @@
 cask "domination" do
-  version "1.2.3"
-  sha256 "e2b2526048123cc838eec1dc96f594b7ae16fce95757fdaadbaaac07a4531a7d"
+  version "1.2.4"
+  sha256 "e097258d9ed738d4f66883c131ca10e5ceab6caf598a1a03dfcf83fff0e02757"
 
   url "https://downloads.sourceforge.net/domination/Domination_#{version}.zip"
   appcast "https://sourceforge.net/projects/domination/rss?path=/Domination"
