43ac3e7832527c988e4732a588575bc162ab0e09
Steffen Kötte <steffen.koette@gmail.com>
2023-04-22 16:03:31 -0700

    Replace app cast with live check

--- Casks/domination.rb
+++ Casks/domination.rb
@@ -3,12 +3,15 @@ cask "domination" do
   sha256 "e097258d9ed738d4f66883c131ca10e5ceab6caf598a1a03dfcf83fff0e02757"
 
   url "https://downloads.sourceforge.net/domination/Domination_#{version}.zip"
-  appcast "https://sourceforge.net/projects/domination/rss?path=/Domination"
   name "yura.net Domination"
   desc "Risk Board Game"
   homepage "http://domination.sourceforge.net/"
 
   suite "Domination"
+  
+  livecheck do
+    url "https://sourceforge.net/projects/domination/rss?path=/Domination"
+  end
 
   caveats do
     depends_on_java
